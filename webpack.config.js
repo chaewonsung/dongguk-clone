e80f8793996f20e7f9d40acb553479e8609e8045
Hwang Chae Won <codnjs2445@naver.com>
2024-10-24 14:07:11 +0900

    배포설정완료

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: {
     main: '/src/js/main.js',
@@ -8,7 +11,46 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     filename: '[name].js',
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html',
+      chunks: ['main'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'pages/qna.html',
+      filename: 'pages/qna.html',
+      chunks: ['qna'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'pages/introduction.html',
+      filename: 'pages/introduction.html',
+      chunks: ['introduction'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'pages/student.html',
+      filename: 'pages/student.html',
+      chunks: ['detail'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'pages/teacher.html',
+      filename: 'pages/teacher.html',
+      chunks: ['detail'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'pages/industry.html',
+      filename: 'pages/industry.html',
+      chunks: ['detail'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/images', to: 'src/images' },
+        { from: 'pages/includes', to: 'pages/includes' },
+      ],
+    }),
+  ],
   mode: 'development',
   module: {
     rules: [
